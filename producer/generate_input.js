07ca387d2c0e3465741f5f90c1342d6e5fdeeb7b
danula-ded <stupishin1597@yandex.ru>
2025-03-25 19:41:48 +0300

    update tasks

--- producer/generate_input.js
+++ producer/generate_input.js
@@ -4,18 +4,24 @@ function generateMessages(count) {
     const messages = [];
     for (let i = 1; i <= count; i++) {
         messages.push(`Message ${i}`);
+        if (i % 3 === 0) {
+            messages.push(`Message ${i}`);
+        }
+        if (i % 4 === 0) {
+            messages.push(`Message ${i}`);
+        }
     }
     return messages.join('\n');
 }
 
 const fileName = 'input.txt';
-const messageCount = 500;
+const messageCount = 100;
 const content = generateMessages(messageCount);
 
 fs.writeFile(fileName, content, (err) => {
     if (err) {
         console.error('Ошибка при создании файла:', err);
     } else {
-        console.log(`Файл "${fileName}" успешно создан и заполнен ${messageCount} строками.`);
+        console.log(`Файл "${fileName}" успешно создан и заполнен ${messageCount} строками. С повторением каждой 3 строки и строк делящихся на 4, для проверки Redis`);
     }
 });
